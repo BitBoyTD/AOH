36c9ac1cc6b1e4f0749e6026b040f2c253f69391
Seán Osborne <123533598+BitBoyTD@users.noreply.github.com>
2023-08-22 18:32:17 +1200

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+// toggle nav buttons function
+function toggleNavButtons() { 
+    const navButtonsSection = document.getElementById("navButtonsSection");
+    const moreButtonImg = document.getElementById("moreButtonImg");
+    if (navButtonsSection.style.display === "none" ||
+    navButtonsSection.style.display === "") {
+        navButtonsSection.style.display = "flex";
+        moreButtonImg.classList.toggle("moreButtonWhite");
+        setTimeout(() => {
+            navButtonsSection.classList.add("navButtons-slide-in");
+        }, 0); // Adding the class immediately after display change
+    } else {
+        navButtonsSection.classList.remove("navButtons-slide-in");
+        moreButtonImg.classList.toggle("moreButtonWhite");
+        setTimeout(() => {
+            navButtonsSection.style.display = "none";
+        }, 500); // Adjust the delay based on your transition duration
+    }
+}
+// moreButton
+moreButton = document.getElementById("moreButton");
+moreButton.addEventListener("click", toggleNavButtons);
