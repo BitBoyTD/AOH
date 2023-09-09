2b0f83714b65eb8c1b6fd9332204132a9a5857b0
Seán Osborne <123533598+BitBoyTD@users.noreply.github.com>
2023-09-09 14:05:18 +1200

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -3,30 +3,18 @@ function toggleNavButtons() {
     const navButtonsSection = document.getElementById("navButtonsSection");
     const moreButton = document.getElementById("moreButton");
     const moreButtonBars = document.querySelectorAll("#moreButton img");
-    const navBar = document.getElementById("navBar");
-    if (navButtonsSection.style.display === "none" ||
-    navButtonsSection.style.display === "") {
-        navButtonsSection.style.display = "flex";
+    if (navButtonsSection.classList[1] !== "navButtons-slide-in") {
         moreButton.classList.toggle("filterWhite");
         moreButtonBars[0].classList.toggle('topBarX');
         moreButtonBars[1].classList.toggle('midBarX');
         moreButtonBars[2].classList.toggle('botBarX');
-        navButtonsSection.style.boxShadow = "0 0 20px";
-        navBar.style.boxShadow = "none";
-        setTimeout(() => {
-            navButtonsSection.classList.add("navButtons-slide-in");
-        }, 0); 
+        navButtonsSection.classList.add("navButtons-slide-in");
     } else {
         navButtonsSection.classList.remove("navButtons-slide-in");
         moreButton.classList.toggle("filterWhite");
         moreButtonBars[0].classList.toggle('topBarX');
         moreButtonBars[1].classList.toggle('midBarX');
         moreButtonBars[2].classList.toggle('botBarX');
-        setTimeout(() => {
-            navButtonsSection.style.display = "none";
-            navButtonsSection.style.boxShadown = "none";
-            navBar.style.boxShadow = "0 0 20px";
-        }, 500); 
     }
 }
 // moreButton
