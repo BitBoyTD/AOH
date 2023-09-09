63dc7d67de27ee3658b9c364517bdff9dc5bc2a3
Seán Osborne <123533598+BitBoyTD@users.noreply.github.com>
2023-09-09 13:28:39 +1200

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,16 @@
 // toggle nav buttons function
 function toggleNavButtons() { 
     const navButtonsSection = document.getElementById("navButtonsSection");
-    const moreButtonImg = document.getElementById("moreButtonImg");
+    const moreButton = document.getElementById("moreButton");
+    const moreButtonBars = document.querySelectorAll("#moreButton img");
     const navBar = document.getElementById("navBar");
     if (navButtonsSection.style.display === "none" ||
     navButtonsSection.style.display === "") {
         navButtonsSection.style.display = "flex";
-        moreButtonImg.classList.toggle("filterWhite");
+        moreButton.classList.toggle("filterWhite");
+        moreButtonBars[0].classList.toggle('topBarX');
+        moreButtonBars[1].classList.toggle('midBarX');
+        moreButtonBars[2].classList.toggle('botBarX');
         navButtonsSection.style.boxShadow = "0 0 20px";
         navBar.style.boxShadow = "none";
         setTimeout(() => {
@@ -14,7 +18,10 @@ function toggleNavButtons() {
         }, 0); 
     } else {
         navButtonsSection.classList.remove("navButtons-slide-in");
-        moreButtonImg.classList.toggle("filterWhite");
+        moreButton.classList.toggle("filterWhite");
+        moreButtonBars[0].classList.toggle('topBarX');
+        moreButtonBars[1].classList.toggle('midBarX');
+        moreButtonBars[2].classList.toggle('botBarX');
         setTimeout(() => {
             navButtonsSection.style.display = "none";
             navButtonsSection.style.boxShadown = "none";
